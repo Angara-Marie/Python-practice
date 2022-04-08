be23213d0bfd5d2ff1f23022b89003c98a72aea7
Angara-Marie <angaramarie@gmail.com>
2022-04-08 14:36:11 +0300

    python code

---
+++ main.py
@@ -0,0 +1,23 @@
+sentence= "I love food"
+print(sentence)
+sentence= "Who are you?"
+print(sentence)
+name= "Tessa anitab"
+print (name.title())
+a="Virgin"
+b="Mojito"
+txt= "{} {}".format(a,b)
+print(txt)
+a= "Moscow"
+b="Mule"
+print(f"{a} {b}")
+x="florida dolphins"
+print(x.capitalize())
+y="Hello World"
+print(y[::-1])
+sentence= "The cat in the hat"
+print(sentence.upper())
+print(sentence.lower())
+city= 'New York'
+print(city.endswith('k'))
+print(city.startswith('N'))
